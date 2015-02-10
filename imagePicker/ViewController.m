b6a9e8b25052fcacb92103509e991cf1f99f86c9
octps <renmo9@gmail.com>
2015-02-10 14:00:53 +0000

    #3 トレーサーの追加・動画のrateの制御

--- imagePicker/ViewController.m
+++ imagePicker/ViewController.m
@@ -25,12 +25,78 @@
 
 @implementation ViewController
 
+// 変数宣言
+UIImageView *ivMe;
+UIImageView *ivTracer;
+
+//画像trace用の変数宣言
+NSTimer *timer = nil;
+
+
 - (void)viewDidLoad {
+    // 画像を表示 me
+    UIImage *imageMe = [UIImage imageNamed:@"me.png"];
+    ivMe = [[UIImageView alloc] initWithImage:imageMe];
+    ivMe.backgroundColor = [UIColor whiteColor];
+    [ivMe setFrame:CGRectMake(50.0, 50.0, imageMe.size.width, imageMe.size.height)];
+    [self.view addSubview:ivMe];
+    
+    // 画像を表示 tracer
+    UIImage *imageTracer = [UIImage imageNamed:@"tracer.png"];
+    ivTracer = [[UIImageView alloc] initWithImage:imageTracer];
+    ivTracer.backgroundColor = [UIColor whiteColor];
+    [ivTracer setFrame:CGRectMake(50.0, 50.0, imageTracer.size.width, imageTracer.size.height)];
+    [self.view addSubview:ivTracer];
+    
+    // tracer用のsetInterval タイマー
+    [NSTimer
+     scheduledTimerWithTimeInterval:0.01
+     target:self
+     selector:@selector(onTimer:)
+     userInfo:nil
+     repeats:YES];
+
     [super viewDidLoad];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+// タッチした時の座標に画像meと画像tracerとを移動
+- (void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint p = [[touches anyObject] locationInView:self.view];
+    ivMe.center = p;
+    ivTracer.center = p;
+}
+
+// タッチしてmoveした時に画像meを移動するメソッド
+- (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    CGPoint p = [[touches anyObject] locationInView:self.view];
+    ivMe.center = p;
+}
+
+// tracer用のメソッド
+- (void)onTimer:(NSTimer *)timer {
+    // meとtracerの距離を出す
+    // 距離を適当な数値でわる
+    // tracerに追加する
+    // 直接代入だとエラー、構造体とクラスの問題らしい。参考は以下
+    // http://info-utakura.blogspot.jp/2011/11/xcodeexpression-is-no-assignable.html
+    
+    CGRect ivTracerFrame = ivTracer.frame;
+    ivTracerFrame.origin.x = ivTracerFrame.origin.x + ((ivMe.center.x - ivTracer.center.x)/20);
+    [ivTracer setFrame:ivTracerFrame];
+    
+    //ここで、距離の問題がはっせい
+    //-2〜2までとする挙動を追加すること
+    [self setupRate:(double) ((ivMe.center.x - ivTracer.center.x)/20)];
+}
+
+//動画の再生速度を決めるメソッド
+- (void)setupRate:(double) value
+{
+    self.player.rate = value;
 }
 
 - (IBAction)selectImageButtonDidTouch:(id)sender {
@@ -103,7 +169,6 @@
 
 - (void)onVideoEnd:(NSNotification *)notification
 {
-    NSLog(@"video end");
     AVPlayerItem *p = [notification object];
     [p seekToTime:kCMTimeZero];
     [self.player play];
